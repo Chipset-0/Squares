c0d15f6b340c3fb262b51f38dc7d9c7ea31d8d8f
Zachary Dennett <73580481+Chipset-0@users.noreply.github.com>
2025-01-21 15:10:14 +0800

    Hover effect completed. Randomized hover colour

--- javascript-index.js
+++ javascript-index.js
@@ -6,19 +6,35 @@ gridSizeButton.addEventListener("click", (event) => {
     promptForNewGridSize()
 })
 
-const squareRowDiv = document.createElement("div")
-squareRowDiv.classList.add("squareRowDiv")
-function updateFlexContainer(gridWidth)
+const clearButton = document.querySelector("#clearButton")
+clearButton.addEventListener("click", (event) => {
+    updateFlexContainer()
+})
+
+let currentGridSize = 16
+
+function getRndRGBVal() {
+    return Math.floor(Math.random() * (256) );
+}
+
+function getRndColor()
 {
-    for (let j = 1; j <= gridWidth**2; j++)
+    
+    return "rgb(" + getRndRGBVal() + "," + getRndRGBVal() + "," + getRndRGBVal() + ")"; 
+}
+
+function updateFlexContainer()
+{
+    containerDiv.innerHTML = ''
+    for (let j = 1; j <= currentGridSize**2; j++)
     {
         let square = document.createElement("div")
         square.classList.add("squareDiv")
-        square.style.width = `${100 / gridWidth}%`;
-        square.style.height = `${100 / gridWidth}%`;
+        square.style.width = `${100 / currentGridSize}%`;
+        square.style.height = `${100 / currentGridSize}%`;
         
         square.addEventListener("mouseenter", (event) => {
-            event.target.style.backgroundColor = "pink"
+            event.target.style.backgroundColor = getRndColor()
         })
         containerDiv.appendChild(square)
     }
@@ -26,7 +42,29 @@ function updateFlexContainer(gridWidth)
 
 function promptForNewGridSize()
 {
-
+    let validInput = false
+    while (!validInput)
+    {
+        let result = parseInt(prompt("What grid size? [1-100]"))
+        if (isNaN(result))
+        {
+            alert("Not a number")
+        }
+        else if (result < 0)
+        {
+            alert("Negative Numbers Not Accepted")
+        }
+        else if (result > 100)
+        {
+            alert("Number must be from 1 to 100 inclusive")
+        }
+        else
+        {
+            currentGridSize = result
+            updateFlexContainer()
+            validInput = true
+        }
+    }
 }
 
-updateFlexContainer(100)
+updateFlexContainer()
